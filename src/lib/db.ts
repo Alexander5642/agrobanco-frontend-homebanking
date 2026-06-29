9b0073588bea427e2713cf4cd7af99c57b9eaa7a
Alexander5642 <166543226+Alexander5642@users.noreply.github.com>
2026-06-29 00:54:35 -0500

    security: move hardcoded db string to env vars

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_EcAFR5vYjlZ9@ep-steep-night-aty4m52i.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require',
+  connectionString: process.env.POSTGRES_URL,
 });
 
 export default pool;
